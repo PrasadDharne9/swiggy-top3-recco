523d33a0784c8ea7af2a3a9d1980976e8f8eedc5
Prasaddharne9 <dharneprasadt@gmail.com>
2026-03-11 18:06:43 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this Projects gives the top 3 recommended food items from the swiggy home page',
+    author='Prasad Dharne',
+    license='',
+)
